b8f67bcebe7af9b1b077d1e54237b12384709547
brianngumbau <brianngumbau9075@gmail.com>
2023-10-29 04:41:54 -0700

    commit

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Addition module.
+it adds 2 integers
+a and b must be first casted if they are float
+"""
+
+
+def add_integer(a, b=98):
+    """a and b are integers
+    Returns an integer: a + b
+    """
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    if not (isinstance(a, int)):
+        raise TypeError("a must be an integer")
+    if not (isinstance(b, int)):
+        raise TypeError("b must be an integer")
+    return (a + b)
