cbc525c2d12a0afd9b7fe46b255625167707a78c
brianngumbau <brianngumbau9075@gmail.com>
2024-01-13 00:16:45 -0800

    update

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-""" takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument"""
+""" takes in an argument and displays all
+values in the states table of hbtn_0e_0_usa where
+name matches the argument"""
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-            passwd=argv[2], db=argv[3], charset="utf8")
+                         passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
     states.id ASC".format(argv[4]))
