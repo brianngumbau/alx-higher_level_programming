8ee7de5e307f189ad8f23e4ad7fc8ef17a560ac9
brianngumbau <brianngumbau9075@gmail.com>
2024-01-13 00:45:02 -0800

    update

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,8 +12,7 @@ if __name__ == "__main__":
     cur.execute("SELECT * FROM states WHERE
                 name LIKE 'N%' ORDER BY states.id ASC")
     rows = cur.fetchall()
-    for row in rows:
-        if row[1][0] == 'N':
-            print(row)
+    for i in rows:
+        print(row)
     cur.close()
     db.close()
