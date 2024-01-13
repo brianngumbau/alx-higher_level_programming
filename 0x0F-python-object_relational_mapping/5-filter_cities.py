108c5806160da8929fac690919ce978a7597c2d6
brianngumbau <brianngumbau9075@gmail.com>
2024-01-12 23:20:36 -0800

    task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -5,18 +5,4 @@ cities of that state, using the database hbtn_0e_4_usa"""
 import MySQLdb
 from sys import argv
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
-    cur = db.cursor()
-    cur.execute("SELECT cities.id,cities.name FROM cities\
-                INNER JOIN states ON cities.state_id = states.id\
-                WHERE states.name = %s", [argv[4])
-    rows = cur.fetchball()
-    k = []
-    for i in rows:
-    k.append(i[1])
-    print(", ".join(k)
-
-    cur.close()
-    db.close()
+if __name__ == "__main__"
