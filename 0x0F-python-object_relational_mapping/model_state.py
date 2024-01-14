4ed22682ba7d5e37d7064dbda9fc8fc8fcf29e2c
brianngumbau <brianngumbau9075@gmail.com>
2024-01-14 00:57:03 -0800

    update

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-"""class definition of a State and an instance Base = declarative_base()
-"""
-from sys import argv
-from model_state import Base, State
-from sqlalchemy import (create_engine)
+""" class definition of a State and an instance Base = declarative_base()"""
 
-if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(argv[1], argv[2],
-                argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """Module of a city"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
