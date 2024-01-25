d99850030fd58214c598a0e1f4feba4b43b4be29
brianngumbau <brianngumbau9075@gmail.com>
2024-01-25 11:03:20 -0800

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,26 +2,11 @@
 """finds a peak in a list of unsorted integers."""
 
 def find_peak(list_of_integers):
-    """
-    Finds peak element in a list of unsorted integers.
-
-    Args:
-    - list_of_integers: list of unsorted integers.
-
-    Returns:
-    - peak element in the list.
-    """
-    if not list_of_integers:
+    if list_of_integers == []:
         return None
-
-    lo, hi = 0, len(list_of_integers) - 1
-
-    while lo < hi:
-        mid = (lo + hi) // 2
-
-        if list_of_integers[mid] > list_of_integers[mid + 1]:
-            hi = mid
-        else:
-            lo = mid + 1
-
-    return list_of_integers[l]
+    else:
+        max_value = list_of_integers[0]
+        for item in list_of_integers:
+            if item > max_value:
+                max_value = item
+        return (max_value)
