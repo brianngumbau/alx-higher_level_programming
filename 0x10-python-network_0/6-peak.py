16a3e7dd65015b688e1be63872699a798c1393ef
brianngumbau <brianngumbau9075@gmail.com>
2024-01-25 11:47:55 -0800

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,6 +2,7 @@
 """finds a peak in a list of unsorted integers"""
 
 def find_peak(list_of_integers):
+    """finds a peak in list_of_integers"""
     if not list_of_integers:
         return None
 
