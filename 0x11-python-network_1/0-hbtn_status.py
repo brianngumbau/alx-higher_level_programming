4cc5a73d2f8ced1bb87aa0fcc3b545f0e27b7478
brianngumbau <brianngumbau9075@gmail.com>
2024-01-26 02:09:32 -0800

    update

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""fetches https://intranet.hbtn.io/status."""
+
+import urllib.request
+with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
