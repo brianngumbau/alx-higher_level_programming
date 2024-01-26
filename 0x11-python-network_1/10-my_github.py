7fc090b8bed50ed6b61bc0ca6222baa13567fbb9
brianngumbau <brianngumbau9075@gmail.com>
2024-01-26 04:31:14 -0800

    task 9

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" takes your GitHub credentials (username and password) and uses the GitHub API to display your id
+"""
+import requests
+from requests.auth import HTTPBasicAuth
+from sys import argv
+
+if __name__ == '__main__':
+    url = 'https://api.github.com/users/{}'.format(argv[1])
+    response = requests.get(url,
+                     auth=HTTPBasicAuth(argv[1], argv[2]))
+    print(response.json().get('id'))
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -30,4 +30,3 @@ if __name__ == "__main__":
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
         sys.exit(1)
-
