4c2441e8874d90bb2e7f70500ee8e85c8205eb3c
brianngumbau <brianngumbau9075@gmail.com>
2024-03-26 13:32:54 -0700

    task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+const episodeNum = process.argv[2];
+const API_URL = 'https://swapi-api.hbtn.io/api/films/';
+
+request(API_URL + episodeNum, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  }
+	const json = JSON.parse(body);
+	console.log(json.title);
+});
